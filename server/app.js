ea21113bcac825c92a3eb2bd72e1d72cb41f560a
Dat Dang <dangdatcao2002@gmail.com>
2023-06-09 16:35:10 +0700

    test ci/cd

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const onlineUsers = [];
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
